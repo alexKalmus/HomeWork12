a00006de9799532ce0a6fdfe65c1f0d0cc656e32
Alex Kalmus <kalmus2111@gmail.com>
2020-11-02 21:14:14 +0200

    countOf

--- src/com/homeWork12/Main.java
+++ src/com/homeWork12/Main.java
@@ -1,8 +1,16 @@
 package com.homeWork12;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s = sc.nextLine();
+        System.out.println("Enter the fragment");
+        String needle = sc.nextLine();
+        int count = StringTasks.countOf(s, needle);
+        System.out.printf("[%s] is count %d times in [%s]",needle,count,s);
     }
 }
--- src/com/homeWork12/StringTasks.java
+++ src/com/homeWork12/StringTasks.java
@@ -1,4 +1,16 @@
 package com.homeWork12;
 
 public class StringTasks {
+    static int countOf (String s, String needle){
+        int index;
+        int count = 0;
+        for (int i = 0; i < s.length() ; i++) {
+            index = s.indexOf(needle, i);
+            if (index >= 0){
+                count++;
+                i = index + 1;
+            }
+        }
+        return count;
+    }
 }
