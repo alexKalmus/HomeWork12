c595e9ba66a02c7666cffb5aa54d7d1d465ea0b3
Alex Kalmus <kalmus2111@gmail.com>
2020-11-02 23:29:54 +0200

    ome Work Completed

--- src/com/homeWork12/Main.java
+++ src/com/homeWork12/Main.java
@@ -6,11 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string");
-        String s = sc.nextLine();
-        System.out.println("Enter the fragment");
-        String needle = sc.nextLine();
-        int count = StringTasks.countOf(s, needle);
-        System.out.printf("[%s] is count %d times in [%s]",needle,count,s);
+//        System.out.println("Enter the string");
+//        String s = sc.nextLine();
+//        System.out.println("Enter the fragment");
+//        String needle = sc.nextLine();
+//        int count = StringTasks.countOf(s, needle);
+//        System.out.printf("[%s] is count %d times in [%s] \n",needle,count,s);
+//        System.out.println("Enter the key of Cesar crypt");
+//        int key = sc.nextInt();
+//        sc.nextLine();
+//        String cesarS = StringTasks.cesarCrypt(s,key);
+//        System.out.printf("String in Cesar crypt: [%s] \n", cesarS);
+        System.out.println("Enter the string of number");
+        Contact[] contacts = StringTasks.parseContacts(sc.nextLine());
+        for (int i = 0; i < contacts.length; i++) {
+            System.out.println(contacts[i].toString());
+        }
+
     }
 }
--- src/com/homeWork12/StringTasks.java
+++ src/com/homeWork12/StringTasks.java
@@ -13,4 +13,32 @@ public class StringTasks {
         }
         return count;
     }
+    static String cesarCrypt (String s, int key){
+        char[] cesarArr = s.toCharArray();
+        char[] firstSymbol = new char[key];
+        for (int i = cesarArr.length - key, j = 0 ; j < key; i++, j++) {
+            firstSymbol[j] = cesarArr [i];
+        }
+        for (int i = cesarArr.length -1; i >= key; i--) {
+                cesarArr[i] = cesarArr[i-key];
+        }
+
+        for (int i = 0; i < firstSymbol.length; i++) {
+            cesarArr[i] = firstSymbol[i];
+        }
+        String cesarS = String.valueOf(cesarArr);
+        return cesarS;
+    }
+
+    static Contact[] parseContacts(String s){
+        String[] namesAndNumbers = s.split(";");
+        Contact[] contacts = new Contact[namesAndNumbers.length];
+        for (int i = 0; i < contacts.length; i++) {
+            String[] simple = namesAndNumbers[i].split(":");
+            simple[0] = simple[0].trim();
+            simple[1] = simple[1].trim();
+            contacts[i] = new Contact(simple[0], simple[1]);
+        }
+        return contacts;
+    }
 }
