4de84c80bc8cfc44e25ecf9d0f3fc0727667c894
Alex Kalmus <kalmus2111@gmail.com>
2020-11-02 23:34:02 +0200

    Home Work Completed

--- src/com/homeWork12/Contact.java
+++ src/com/homeWork12/Contact.java
@@ -11,21 +11,6 @@ public class Contact {
         this.number = number;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
 
     @Override
     public boolean equals(Object o) {
